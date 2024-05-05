93a525ebc352c2f608e50624216ce36f637d7cdd
Nicholas <bestnicholas05@gmail.com>
2024-05-05 15:51:03 -0400

    Completed Arrow Functions Lab

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = (number1, number2) => number1 / number2
+divide(2000,100);
+const square = (x) => x * x;
+square(2)
+const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+  }
+  add(3, 4);
